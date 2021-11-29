067405b3e5358d4d81094d01e4ad4e8044afbdf6
AndreFGSilva <andrenenesilvafcp13@gmail.com>
2021-11-29 23:33:03 +0000

    Changes in invariantes.pl.

--- src/invariantes.pl
+++ src/invariantes.pl
@@ -9,38 +9,45 @@
 % ------------------------------------------
 % Definições iniciais
 :- dynamic estafeta/3.
-:- dynamic cliente/5.
-:- dynamic encomenda/9.
+:- dynamic cliente/2.
+:- dynamic pedido/11.
+:- dynamic encomenda/3.
 
 % ------------------------------------------
 % Invariantes estruturais para o predicado estafeta.
 % Conjunto de encomendas: lista com todos os IDs das encomendas.
 % Penalização: inteiro que funciona como contador, sempre que não entregar, adiciona uma penalização.
-+estafeta(ID,Encomendas,Penalizacao) :: (integer(ID)
-                                        ,validaListaInteger(Encomendas)
++estafeta(ID,Pedidos,Penalizacao) ::    (integer(ID)
+                                        ,validaListaInteger(Pedidos)
                                         ,integer(Penalizacao)).
+
+% ------------------------------------------
+% Invariantes estruturais para o predicado cliente.
++cliente(IDCliente,IDPedido) :: (integer(IDCliente)
+                                ,integer(IDPedido)).
 
 % ------------------------------------------
 % Invariantes estruturais para o predicado cliente.
 % Prazo de entrega: imediato , 2h, 6h, 1 dia, 3 dias, 7 dias.
 % Classificação da entrega: 0 a 5.
-+cliente(IDCliente,IDEncomenda,IDEstafeta,Prazo,Classificacao) ::   (integer(IDCliente)
-                                                                    ,integer(IDEncomenda)
-                                                                    ,integer(IDEstafeta)
-                                                                    ,validaPrazo(Prazo)
-                                                                    ,validaClassificacao(Classificacao)).
+% Transporte: bicicleta - 1 , moto - 2 , carro - 3.
+% Preço: calculado através de peso, volume, transporte utilizado (menos ecológico = mais caro) e o prazo de entrega (mais curto = mais caro).
++pedido(IDPedido,IDCliente,IDEncomenda,IDEstafeta,Rua,Freguesia,Data,Prazo,Classificacao,Transporte,Preco) ::   (integer(IDPedido)
+                                                                                                                ,integer(IDCliente)
+                                                                                                                ,integer(IDEncomenda)
+                                                                                                                ,integer(IDEstafeta)
+                                                                                                                ,atom(Rua)
+                                                                                                                ,atom(Freguesia)
+                                                                                                                ,validaData(Data)
+                                                                                                                ,validaPrazo(Prazo)
+                                                                                                                ,validaClassificacao(Classificacao)
+                                                                                                                ,validaTransporte(Transporte)
+                                                                                                                ,calculaPreco(Preco)).
 
 % ------------------------------------------
 % Invariante estrutural para o predicado encomenda.
-% Preço: calculado através de peso, volume, transporte utilizado (menos ecológico = mais caro) e o prazo de entrega (mais curto = mais caro).
-+encomenda(IDEncomenda,IDCliente,Rua,Freguesia,Peso,Volume,Data,Transporte,Preco) ::    (integer(IDEncomenda)
-                                                                                        ,integer(IDCliente)
-                                                                                        ,atom(Rua)
-                                                                                        ,atom(Freguesia)
-                                                                                        ,integer(Peso)
-                                                                                        ,integer(Volume)
-                                                                                        ,validaData(Data)
-                                                                                        ,validaTransporte(Transporte)
-                                                                                        ,calculaPreco(Preco)).
++encomenda(IDEncomenda,Peso,Volume) ::  (integer(IDEncomenda)
+                                        ,integer(Peso)
+                                        ,integer(Volume)).
 
 % ------------------------------------------
